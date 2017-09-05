b6811e13f0017c6c20d0d1c2736452949b4e9b98
Firefly4266 <gaines.johnp@gmail.com>
2017-09-05 15:46:30 -0700

    Back to starter code

--- CoinFlip/Program.cs
+++ CoinFlip/Program.cs
@@ -10,32 +10,6 @@ namespace CoinFlip
     {
         static void Main(string[] args)
         {
-            int coin;
-            string userGuess;
-            Random numberGenerator = new Random();
-
-            //query user
-            Console.Write("enter your guess, Heads or Tails (H or T): ");
-            userGuess = Console.ReadLine();
-
-            //get random number
-            coin = numberGenerator.Next(0, 2);
-            if (coin == 0 && userGuess == "T")
-            {
-                Console.WriteLine("The flip was Tails, you win!!!");
-            }
-            else if (coin == 1 && userGuess == "H")
-            {
-                Console.WriteLine("The flip was Heads, you win!!!");
-            }
-            else if (coin == 0 && userGuess == "H")
-            {
-                Console.WriteLine("Sorry, the flip was Tails...you lose.");
-            }
-            else
-            {
-                Console.WriteLine("Sorry, the flip was Heads...you lose.");
-            }
         }
     }
 }
